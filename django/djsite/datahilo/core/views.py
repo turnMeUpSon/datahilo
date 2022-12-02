2060b2e9580bdb5cdb32526e0ac9e8b25cc2a05f
turnMeUpSon <milleryan2003@gmail.com>
2022-12-02 17:39:11 +0300

    Keep adding indicators to analytics dashboard

--- django/djsite/datahilo/core/views.py
+++ django/djsite/datahilo/core/views.py
@@ -53,3 +53,68 @@ def get_treasury_y_10_minus_y2(request):
 def get_motor_vehicle_retail_sales(request):
     motor_vehicle_retail_sales_heavy_weight_trucks = Indicators.objects.get(pk=2)
     return render(request, 'core/motor_vehicle_retail_sales.html', {'motor_vehicle_retail_sales_heavy_weight_trucks': motor_vehicle_retail_sales_heavy_weight_trucks})
+
+
+def get_unemployment_rate(request):
+    unemployment_rate = Indicators.objects.get(pk=3)
+    return render(request, 'core/unemployment_rate.html', {'unemployment_rate': unemployment_rate})
+
+
+def get_federal_funds_effective_rate_and_cpi_usa(request):
+    federal_funds_effective_rate_and_cpi_usa = Indicators.objects.get(pk=4)
+    return render(request, 'core/federal_funds_effective_rate_and_cpi_usa.html', {'federal_funds_effective_rate_and_cpi_usa': federal_funds_effective_rate_and_cpi_usa})
+
+
+def get_treasury_y10_minus_m3(request):
+    treasury_y10_minus_m3 = Indicators.objects.get(pk=5)
+    return render(request, 'core/treasury_y10_minus_m3.html', {'treasury_y10_minus_m3': treasury_y10_minus_m3})
+
+
+def get_global_price_of_food_index(request):
+    global_price_of_food_index = Indicators.objects.get(pk=6)
+    return render(request, 'core/global_price_of_food_index.html', {'global_price_of_food_index': global_price_of_food_index})
+
+
+def get_natural_gas_liquid_composite_price_usa(request):
+    natural_gas_liquid_composite_price_usa = Indicators.objects.get(pk=7)
+    return render(request, 'core/natural_gas_liquid_composite_price_usa.html', {'natural_gas_liquid_composite_price_usa': natural_gas_liquid_composite_price_usa})
+
+
+def get_real_gross_domestic_product(request):
+    real_gross_domestic_product = Indicators.objects.get(pk=8)
+    return render(request, 'core/real_gross_domestic_product.html', {'real_gross_domestic_product': real_gross_domestic_product})
+
+
+def get_industrial_production_and_capacity_utilization(request):
+    industrial_production_and_capacity_utilization = Indicators.objects.get(pk=9)
+    return render(request, 'core/industrial_production_and_capacity_utilization.html', {'industrial_production_and_capacity_utilization': industrial_production_and_capacity_utilization})
+
+
+def get_initial_claims(request):
+    initial_claims = Indicators.objects.get(pk=10)
+    return render(request, 'core/initial_claims.html', {'initial_claims': initial_claims})
+
+
+def get_real_personal_consumption_expenditures(request):
+    real_personal_consumption_expenditures = Indicators.objects.get(pk=11)
+    return render(request, 'core/real_personal_consumption_expenditures.html', {'real_personal_consumption_expenditures': real_personal_consumption_expenditures})
+
+
+def get_advance_retail_sales_retail_trade(request):
+    advance_retail_sales_retail_trade = Indicators.objects.get(pk=12)
+    return render(request, 'core/advance_retail_sales_retail_trade.html', {'advance_retail_sales_retail_trade': advance_retail_sales_retail_trade})
+
+
+def get_merchant_wholesalers_sales(request):
+    merchant_wholesalers_sales = Indicators.objects.get(pk=13)
+    return render(request, 'core/merchant_wholesalers_sales.html', {'merchant_wholesalers_sales': merchant_wholesalers_sales})
+
+
+def get_total_business_inventories(request):
+    total_business_inventories = Indicators.objects.get(pk=14)
+    return render(request, 'core/total_business_inventories.html', {'total_business_inventories': total_business_inventories})
+
+
+def get_total_consumer_credit_outstanding_usa(request):
+    total_consumer_credit_outstanding_usa = Indicators.objects.get(pk=15)
+    return render(request, 'core/total_consumer_credit_outstanding_usa.html', {'total_consumer_credit_outstanding_usa': total_consumer_credit_outstanding_usa})
