2316e45ccaea87599036b89a5218bba0675608b0
turnMeUpSon <milleryan2003@gmail.com>
2022-12-06 14:55:18 +0300

    Made web security operations

--- django/djsite/datahilo/datahilo/settings.py
+++ django/djsite/datahilo/datahilo/settings.py
@@ -1,18 +1,15 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*1*8(xv2to0+z!s)++q5k1m8w-!3zy3e8$5k4r5uq*etka-bl!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -74,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'datahilo',
         'USER': 'admin',
-        'PASSWORD': '1!2@3#4$YanMiller',
+        'PASSWORD': config('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
